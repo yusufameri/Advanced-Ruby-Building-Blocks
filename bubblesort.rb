b0ee701d8a131b0ad9904ae4ede9b72287ab3b45
Yusuf Ameri <yusuf.americianaki@gmail.com>
2016-01-09 19:34:58 +0330

    implemented bubble_sort_by

--- bubblesort.rb
+++ bubblesort.rb
@@ -18,7 +18,26 @@ def bubble_sort items
   end #end until
   items
 end # end method
+puts "bubble_sort: "
 puts bubble_sort [4,5,3,8,4,1,4]
 # puts bubble_sort [1,2,3]
 # puts swap [1,5,3,7,4,2],2,5
 
+
+def bubble_sort_by items
+	n = items.length
+	swaped = true
+	while swaped do
+		swaped = false
+    for i in (1..n-1)
+    	if yield(items[i], items[i-1])
+    		items = swap items, i-1, i
+    		swaped = true
+    	end # end if
+    end# end for
+    n-=1
+  end #end until
+  items
+end # end method
+puts "bubble_sort_by: "
+puts bubble_sort_by [4,5,3,8,4,1,4] {|first, second| first < second}
